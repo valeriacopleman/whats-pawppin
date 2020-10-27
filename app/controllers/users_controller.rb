9e9bcc3c70bad2a10010a894c7afcb9a06de8170
valeriacopleman <valeriacopleman@gmail.com>
2020-10-27 11:10:23 -0700

    add views and get routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
     
+    get '/signup' do
+        if logged_in?
+          redirect to "/users/#{current_user.id}"
+        else
+          erb :'/users/create_user'
+        end
+    end
+    
+    get '/login' do
+        if logged_in?
+          redirect to "/users/#{current_user.id}"
+        else
+          erb :'/users/login_user'
+        end
+      end
+    
+
 end
